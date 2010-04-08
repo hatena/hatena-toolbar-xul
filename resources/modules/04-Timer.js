1db59506cf46eb34a1dda5c3225a3b043969add5
nanto_vi <nanto@moon.email.ne.jp>
2010-04-09 08:20:33 +0900

    Timerを追加。

---
+++ resources/modules/04-Timer.js
@@ -0,0 +1,43 @@
+Components.utils.import('resource://hatenabar/modules/00-core.js');
+loadPrecedingModules();
+
+const EXPORTED_SYMBOLS = ['Timer'];
+
+function Timer(interval, repeatCount) {
+    this.interval = interval || 60;
+    this.repeatCount = repeatCount || Infinity;
+    this.currentCount = 0;
+    this.timer = null;
+}
+
+EventService.bless(Timer.prototype);
+
+extend(Timer.prototype, {
+    get isRunning Timer_get_isRunning() !!this.timer,
+
+    start: function Timer_start() {
+        if (this.timer) return;
+        this.timer = new NativeTimer(this, this.interval,
+                                     Ci.nsITimer.TYPE_REPEATING_PRECISE);
+    },
+
+    stop: function Timer_stop() {
+        if (!this.timer) return;
+        this.timer.cancel();
+        this.timer = null;
+    },
+
+    reset: function Timer_reset() {
+        this.stop();
+        this.currentCount = 0;
+    },
+
+    observe: function Timer_observe(subject, topic, data) {
+        this.currentCount++;
+        this.dispatch('timer');
+        if (this.currentCount >= this.repeatCount) {
+            this.stop();
+            this.dispatch('timerComplete');
+        }
+    },
+});
