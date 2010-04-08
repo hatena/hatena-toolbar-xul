93bd9041968f183d1d666ebb77579aefdf9ec02c
nanto_vi <nanto@moon.email.ne.jp>
2010-04-08 15:10:53 +0900

    含むアンテナ、含むブックマーク等のボタンを追加。

---
+++ chrome/content/hatenabar/31-Toolbar.js
@@ -0,0 +1,17 @@
+const EXPORT = ['Toolbar'];
+
+var Toolbar = {
+    openRelatedLink: function Tb_openRelatedLink(link, event) {
+        let context = { url: content.location.href };
+        Command.openContentLinkWith(link, context, event);
+    },
+
+    goReferInDiary: function Tb_goReferInDiary(event) {
+        Command.goRefer('d:refer', content.document, event);
+    },
+
+    goReferInGroup: function Tb_goReferInGroup(event) {
+        let group = Prefs.hatenabar.get('group.selected');
+        Command.goRefer('g:' + group + ':refer', content.document, event);
+    },
+};
