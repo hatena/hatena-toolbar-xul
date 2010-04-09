b05a178e281d113a8e9ceb2f9ab245e050f11558
nanto_vi <nanto@moon.email.ne.jp>
2010-04-09 14:04:13 +0900

    ウィンドウ依存のタイマーをいったん削除。

--- chrome/content/common/01-utils.js
+++ chrome/content/common/01-utils.js
@@ -69,53 +69,6 @@ var getHistoryNodeByURL = function getHistoryNodeByURL(url) {
     return;
 }
 
-// Timer
-
-var Timer = function(interval, repeatCount) {
-    EventService.implement(this);
-    this.currentCount = 0;
-    this.interval = interval || 60; // ms
-    this.repeatCount = repeatCount || 0;
-}
-
-Timer.prototype = {
-    start: function() {
-        this._running = true;
-        this.loopTimer = setTimeout(function(self) {
-            self.loop();
-        }, this.interval, this);
-    },
-    reset: function() {
-        this.stop();
-        this.currentCount = 0;
-    },
-    stop: function() {
-        this.clearTimer();
-        this._running = false;
-    },
-    clearTimer: function() {
-        if (this.loopTimer) {
-            clearTimeout(this.loopTimer);
-            delete this.loopTimer;
-        }
-    },
-    get running() this._running,
-    loop: function() {
-        if (!this.running) return;
-        this.currentCount++;
-        if (this.repeatCount && this.currentCount >= this.repeatCount) {
-            this.stop();
-            this.dispatch('timer');
-            this.dispatch('timerComplete');
-            return;
-        }
-        this.dispatch('timer');
-        this.loopTimer = setTimeout(function(self) {
-            self.loop();
-        }, this.interval, this);
-    },
-}
-
 
 var async = {};
 
