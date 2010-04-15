3769bf73b2c9753c2d58e6aba735b6672896b35d
nanto_vi <nanto@moon.email.ne.jp>
2010-04-15 21:41:37 +0900

    ツールバーのグループボタンのポップアップに参加グループ一覧を表示。

--- chrome/content/hatenabar/31-Toolbar.js
+++ chrome/content/hatenabar/31-Toolbar.js
@@ -6,7 +6,7 @@ var Toolbar = {
     openAntenna: function Tb_openAntenna(event) {
         let link = 'a:';
         if (User.user) {
-            let mode = event.target.value || '';
+            let mode = UICommand.getTarget(event).value || '';
             if (this.antennaModes.indexOf(mode) === -1)
                 mode = User.user.prefs.get('antenna.selected', 'antenna');
             link += 'id:$' + ((mode === 'antenna') ? '' : ':' + mode);
@@ -27,6 +27,50 @@ var Toolbar = {
             else
                 menuitem.removeAttribute('checked');
         });
+        Control.updateLinkPopup(event);
+    },
+
+    openGroup: function Tb_openGroup(event) {
+        let link = 'g:';
+        if (User.user) {
+            let group = UICommand.getTarget(event).value ||
+                        User.user.prefs.get('group.selected', '');
+            if (group) {
+                link += group + ':id:$';
+                User.user.prefs.set('group.selected', group);
+            } else {
+                link += 'id:$:group';
+            }
+        }
+        Command.openUILink(link, event);
+    },
+
+    updateGroupPopup: function Tb_updateGroupPopup(event) {
+        let popup = event.currentTarget;
+        let radios = popup.getElementsByAttribute('name', 'hatenabar-group-id');
+        Array.slice(radios).forEach(function (menuitem) {
+            menuitem.parentNode.removeChild(menuitem);
+        });
+        if (User.user && User.user.groups) {
+            let separator = popup.firstChild;
+            let selected = User.user.prefs.get('group.selected', '');
+            User.user.groups.forEach(function (group) {
+                let menuitem = document.createElementNS(XUL_NS, 'menuitem');
+                menuitem.setAttribute('type', 'radio');
+                menuitem.setAttribute('name', 'hatenabar-group-id');
+                if (group === selected)
+                    menuitem.setAttribute('checked', 'true');
+                menuitem.setAttribute('label', group);
+                menuitem.setAttribute('observes', 'hatenabar-cmd-open-group');
+                menuitem.setAttribute('value', group);
+                menuitem.setAttributeNS(HATENA_NS, 'hatena:link',
+                                        'g:' + group + ':id:$');
+                popup.insertBefore(menuitem, separator);
+            });
+            separator.collapsed = false;
+        } else {
+            popup.firstChild.collapsed = true;
+        }
         Control.updateLinkPopup(event);
     },
 
