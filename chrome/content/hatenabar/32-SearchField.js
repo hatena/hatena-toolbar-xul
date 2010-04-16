395c93a375cd83c37cbb83c5c2cf91498278525c
nanto_vi <nanto@moon.email.ne.jp>
2010-04-16 19:10:40 +0900

    nit: コメントアウトされてた不要部分を削除。

--- chrome/content/hatenabar/32-SearchField.js
+++ chrome/content/hatenabar/32-SearchField.js
@@ -11,6 +11,7 @@ var SearchField = {
         let textbox = this.textbox;
         if (!textbox) return;
         textbox.searchParam = this.inputHistory.key;
+
         let inputBox = document.getAnonymousElementByAttribute(textbox, 'anonid', 'textbox-input-box');
         let contextMenu = document.getAnonymousElementByAttribute(inputBox, 'anonid', 'input-box-contextmenu');
         contextMenu.appendChild(document.createElementNS(XUL_NS, 'menuseparator'));
@@ -19,6 +20,17 @@ var SearchField = {
         //menu.setAttribute('accesskey', '');
         menu.setAttribute('oncommand', "hatenabar.Command.clearSearchHistory();");
         contextMenu.appendChild(menu);
+
+        //let item = textbox.parentNode;
+        //if (item.nextSibling) {
+        //    let splitter = document.createElementNS(XUL_NS, 'splitter');
+        //    splitter.setAttribute('id', 'hatenabar-search-splitter');
+        //    splitter.setAttribute('resizebefore', 'closest');
+        //    splitter.setAttribute('resizeafter', 'flex');
+        //    splitter.setAttribute('tooltiptext', '{{Resize Search Field}}');
+        //    splitter.appendChild(document.createElementNS(XUL_NS, 'toolbarseparator'));
+        //    item.parentNode.insertBefore(splitter, item.nextSibling);
+        //}
 
         this.enableHistory();
     },
