e1e974a591893adb9132c5b39adf5e109297b002
nanto_vi <nanto@moon.email.ne.jp>
2010-04-27 01:05:42 +0900

    ツールバーの移行時にA!の類のボタンを全表示。

--- chrome/content/hatenabar/08-BrowserMigration.js
+++ chrome/content/hatenabar/08-BrowserMigration.js
@@ -23,12 +23,46 @@ BrowserMigration.processes = [
             history.split('||').forEach(function (w) searchbarHistory.add(w));
         }
 
-        document.addEventListener('DOMContentLoaded', applyToolbarSet, false);
-        function applyToolbarSet() {
-            let toolbarSet = shared.get('migration.toolbarSet');
+        // Toolbar Items
+        document.addEventListener('DOMContentLoaded', migrateToolbar, false);
+        function migrateToolbar() {
             let toolbar = byId('hatenabar-toolbar');
-            if (!toolbarSet || !toolbar) return;
-            toolbar.currentSet = toolbarSet;
+            if (!toolbar) return;
+            let toolbarSet = ['hatenabar-main-button'];
+            let toolbarButtonProps = [
+                ['search.enable', true, 'hatenabar-search-item'],
+                ['', true, 'separator'],
+                ['antenna.enable', true, 'hatenabar-antenna-button'],
+                ['bookmark.enable', true, 'hatenabar-bookmark-button'],
+                ['diary.enable', true, 'hatenabar-diary-button'],
+                ['fotolife.enable', true, 'hatenabar-fotolife-button'],
+                ['group.enable', true, 'hatenabar-group-button'],
+                ['haiku.enable', true, 'hatenabar-haiku-button'],
+                ['idea.enable', true, 'hatenabar-idea-button'],
+                ['keyword.enable', false, 'hatenabar-keyword-button'],
+                ['message.enable', false, 'hatenabar-message-button'],
+                ['question.enable', true, 'hatenabar-question-button'],
+                ['star.enable', false, 'hatenabar-star-button'],
+                ['ugomemo.enable', true, 'hatenabar-ugomemo-button'],
+                ['counter.enable', true, 'hatenabar-counter-button'],
+                ['graph.enable', true, 'hatenabar-graph-button'],
+                ['', true, 'separator'],
+                ['', true, 'hatenabar-including-antenna-button'],
+                ['', true, 'hatenabar-add-antenna-button'],
+                ['', true, 'hatenabar-view-bookmark-button'],
+                ['', true, 'hatenabar-add-bookmark-button'],
+                ['', true, 'hatenabar-referring-diary-button'],
+                ['', true, 'hatenabar-diary-refer-button'],
+                ['', true, 'hatenabar-group-refer-button'],
+                ['antenna.included', true, 'hatenabar-check-antenna-item'],
+                ['bookmark.entry', true, 'hatenabar-check-bookmark-item'],
+                ['diary.referred', true, 'hatenabar-check-diary-item'],
+            ];
+            toolbarButtonProps.forEach(function ([key, defaultShow, id]) {
+                if (!key || oldPrefs.get(key, defaultShow))
+                    toolbarSet.push(id);
+            });
+            toolbar.currentSet = toolbarSet.join(',');
             document.persist(toolbar.id, 'currentset');
         }
     },
--- resources/modules/09-Migration.js
+++ resources/modules/09-Migration.js
@@ -62,44 +62,6 @@ Migration.processes = [
         let searchLink = oldPrefs.get('bookmarksearch', false)
                          ? 'b:search' : 'search:search'
         prefs.set('searchbar.link', searchLink);
-
-        // Toolbar
-        let toolbarSet = ['hatenabar-main-button'];
-        let toolbarButtonProps = [
-            ['search.enable', true, 'hatenabar-search-item'],
-            ['', true, 'separator'],
-            ['antenna.enable', true, 'hatenabar-antenna-button'],
-            ['bookmark.enable', true, 'hatenabar-bookmark-button'],
-            ['diary.enable', true, 'hatenabar-diary-button'],
-            ['fotolife.enable', true, 'hatenabar-fotolife-button'],
-            ['group.enable', true, 'hatenabar-group-button'],
-            ['haiku.enable', true, 'hatenabar-haiku-button'],
-            ['idea.enable', true, 'hatenabar-idea-button'],
-            ['keyword.enable', false, 'hatenabar-keyword-button'],
-            ['message.enable', false, 'hatenabar-message-button'],
-            ['question.enable', true, 'hatenabar-question-button'],
-            ['star.enable', false, 'hatenabar-star-button'],
-            ['ugomemo.enable', true, 'hatenabar-ugomemo-button'],
-            ['counter.enable', true, 'hatenabar-counter-button'],
-            ['graph.enable', true, 'hatenabar-graph-button'],
-        ];
-        toolbarButtonProps.forEach(function ([oldKey, showDefault, buttonId]) {
-            if (!oldKey || oldPrefs.get(oldKey, showDefault))
-                toolbarSet.push(buttonId);
-        });
-        let checkersSet = [];
-        let checkerProps = [
-            ['antenna.included', 'hatenabar-check-antenna-item'],
-            ['bookmark.entry', 'hatenabar-check-bookmark-item'],
-            ['diary.referred', 'hatenabar-check-diary-item'],
-        ];
-        checkerProps.forEach(function ([oldKey, buttonId]) {
-            if (oldPrefs.get(oldKey, true))
-                checkersSet.push(buttonId);
-        });
-        if (checkersSet.length)
-            toolbarSet = toolbarSet.concat('separator', checkersSet);
-        shared.set('migration.toolbarSet', toolbarSet.join(','));
     },
 ];
 
