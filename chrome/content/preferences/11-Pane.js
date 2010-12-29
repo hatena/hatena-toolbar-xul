fd9eabb1f6340635c025e5b09edfcd303c091cfe
nanto_vi <nanto@moon.email.ne.jp>
2010-12-29 20:17:05 +0900

    設定画面を開くとエラーが出ていたのを修正。

--- chrome/content/preferences/11-Pane.js
+++ chrome/content/preferences/11-Pane.js
@@ -1,17 +1,10 @@
 const EXPORT = ['Pane'];
 
-const ExtensionManager = getService('@mozilla.org/extensions/manager;1',
-                                    Ci.nsIExtensionManager);
-
-const BOOKMARK_EXTENSION_ID = 'bookmark@hatena.ne.jp';
-
 var Pane = {
     initMoreConvenientPane: function Pane_initMoreConvenientPane() {
         let win = getTopWin();
         // XXX 拡張が有効か無効かも見る。
-        let isBExtInstalled = win
-            ? (typeof win.hBookmark !== 'undefined')
-            : !!ExtensionManager.getItemForId(BOOKMARK_EXTENSION_ID);
+        let isBExtInstalled = !!win && (typeof win.hBookmark !== 'undefined');
         byId('bookmark.useHatenaBookmarkExtension').disabled = !isBExtInstalled;
     },
 };
